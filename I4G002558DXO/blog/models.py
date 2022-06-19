e2c67e4dbe53131e70439947f84873388778e950
OLOWOOKEREISAAC <isaccolowookere1@gmail.com>
2022-06-19 15:19:42 +0100

    finally added author

--- I4G002558DXO/blog/models.py
+++ I4G002558DXO/blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 
 class Post(models.Model):
     Title = models.CharField(max_length=200, null=True)
-    Text = models.TextField()
-    # # Author = models.ForeignKey(get_user_model(), on_delete=)
+    Text = models.TextField(null=True)
+    Author = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
     Created_date = models.DateTimeField(auto_now_add=True, blank=True)
     Published_date = models.DateTimeField(auto_now_add=False, blank=True)
