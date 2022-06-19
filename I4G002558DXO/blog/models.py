a9737dad60f13a475e41363d068e42d83338701c
OLOWOOKEREISAAC <isaccolowookere1@gmail.com>
2022-06-19 15:01:47 +0100

    another commit

--- I4G002558DXO/blog/models.py
+++ I4G002558DXO/blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 
 class Post(models.Model):
     Title = models.CharField(max_length=200, null=True)
-    Text = models.TextField(null=True)
+    Text = models.TextField()
     # # Author = models.ForeignKey(get_user_model(), on_delete=)
-    # Created_date = models.DateTimeField(default=datetime)
-    # Published_date = models.DateTimeField(default=datetime)
+    Created_date = models.DateTimeField(auto_now_add=True, blank=True)
+    Published_date = models.DateTimeField(auto_now_add=False, blank=True)
